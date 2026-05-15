a7306170624771f3f5ac1362b831fab5c7e875f5
isaSanchez-png <isabela.sanchez.mol@gmail.com>
2026-05-15 12:25:34 -0500

    Initial Commit

---
+++ Tip Calculator/Tip_CalculatorApp.swift
@@ -0,0 +1,17 @@
+//
+//  Tip_CalculatorApp.swift
+//  Tip Calculator
+//
+//  Created by Isa on 15/05/26.
+//
+
+import SwiftUI
+
+@main
+struct Tip_CalculatorApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
